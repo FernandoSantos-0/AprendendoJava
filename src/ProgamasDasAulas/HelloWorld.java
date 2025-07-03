4b0987dc133a17a727e91675202676ef2806c213
Fernando-Pereira-Dos-Santos <fp354575@gmail.com>
2025-07-03 17:51:01 -0300

    Initial commit

---
+++ src/ProgamasDasAulas/HelloWorld.java
@@ -0,0 +1,10 @@
+package ProgamasDasAulas;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        String Nome = "Hello world!";
+        System.out.println(Nome);
+
+    }
+}
