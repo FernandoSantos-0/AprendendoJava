35e8eece39ba4083e8ad45ffc3af282e3fc1042d
Fernando-Pereira-Dos-Santos <fp354575@gmail.com>
2025-07-04 17:48:35 -0300

    Saida de dados.

--- src/progamasdasaulas/entradasaidadedado/EntradaESaidaDeDado.java
+++ src/progamasdasaulas/entradasaidadedado/EntradaESaidaDeDado.java
@@ -1,6 +1,7 @@
 package progamasdasaulas.entradasaidadedado;
 
 import java.util.Locale;
+import java.util.Scanner;
 
 public class EntradaESaidaDeDado {
     public static void main(String[] args) {
@@ -10,5 +11,25 @@ public class EntradaESaidaDeDado {
         System.out.println(y);
         System.out.println(x);
         System.out.printf("%.2f%n",x);
+
+        Scanner sc = new Scanner(System.in);
+
+        String palavra;
+        palavra = sc.next();
+
+        System.out.println("A palavra digitada foi "+palavra);
+
+        int i;
+        i = sc.nextInt();
+
+        System.out.println("O numero digitado foi: "+i);
+
+        sc.nextLine();
+        String linha;
+        linha = sc.nextLine();
+
+        System.out.println("O oque foi digitado foi: "+linha);
+
+        sc.close();
     }
 }
