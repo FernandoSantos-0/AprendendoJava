0cf54b96d9f900e2b9ef705157220b91fea80312
Fernando-Pereira-Dos-Santos <fp354575@gmail.com>
2025-08-04 15:10:45 -0300

    Interface.

---
+++ src/aula16/exemplo01/entities/Vehicle.java
@@ -0,0 +1,20 @@
+package aula16.exemplo01.entities;
+
+public class Vehicle {
+
+    private String name;
+
+    public Vehicle (){}
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/aula16/exemplo01/services/TaxService.java
@@ -0,0 +1,6 @@
+package aula16.exemplo01.services;
+
+public interface TaxService {
+
+    double tax(double amount);
+}
