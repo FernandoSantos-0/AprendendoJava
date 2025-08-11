7606741897ef24b9d533d658a254c640a9e3421e
Fernando-Pereira-Dos-Santos <fp354575@gmail.com>
2025-08-11 11:45:03 -0300

    Generics.

---
+++ src/aula17/generics/PrintService.java
@@ -0,0 +1,34 @@
+package aula17.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//  O <Letra qualquer> permite que parametrize a classe com o tipo da instanciacão.
+
+public class PrintService<T> {
+
+    private List<T> list = new ArrayList<>();
+
+    public void addValue(T value) {
+        list.add(value);
+    }
+
+    public T first() {
+        if (list.isEmpty()) {
+            throw new IllegalStateException("List is empty");
+        }
+        return list.get(0);
+    }
+
+    public void print() {
+        System.out.print("[");
+        if (!list.isEmpty()) {
+            System.out.print(list.get(0));
+        }
+        for (int i = 1; i < list.size(); i++) {
+            System.out.print(", " + list.get(i));
+        }
+        System.out.println("]");
+    }
+
+}
